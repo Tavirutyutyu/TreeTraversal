a390dfa31a95c4e41771e491de096a3e304820fb
tavirutyutyu <erdosmarci@gmail.com>
2025-06-06 13:31:53 +0200

    tree wlaker starter code

---
+++ TreeWalker/src/main/java/org/tavirutyutyu/treewalk/TreeWalkerApplication.java
@@ -0,0 +1,13 @@
+package org.tavirutyutyu.treewalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeWalkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TreeWalkerApplication.class, args);
+    }
+
+}
