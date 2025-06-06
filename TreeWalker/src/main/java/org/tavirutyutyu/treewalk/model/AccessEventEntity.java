86868b8f8892f84bb9d151bbc205b35266f2dc3f
tavirutyutyu <erdosmarci@gmail.com>
2025-06-06 14:50:36 +0200

    entity classes added

--- TreeWalker/src/main/java/org/tavirutyutyu/treewalk/model/AccessEventEntity.java
+++ TreeWalker/src/main/java/org/tavirutyutyu/treewalk/model/AccessEventEntity.java
@@ -1,8 +1,25 @@
 package org.tavirutyutyu.treewalk.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "access_event")
 public class AccessEventEntity {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    @Column(name = "access_time")
+    private LocalDateTime accessTime;
+    private String request;
 }
